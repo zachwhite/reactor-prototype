918689e83c1446a4b84514afda9892fadf150525
Zachary White <zach.salter@sojostudios.com>
2013-11-25 13:14:42 -0800

    Basic reactor slot scripts.

---
+++ Assets/Scripts/Components/Slots/Slot.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(SpriteRenderer))]
+public class Slot : MonoBehaviour 
+{
+#region Fields
+	[SerializeField] private Sprite[] _spriteLibrary;
+
+	private SpriteRenderer _spriteRenderer;
+#endregion
+
+
+#region Properties
+	public Sprite[] SpriteLibrary
+	{
+		get { return _spriteLibrary; }
+		set { _spriteLibrary = value; }
+	}
+#endregion
+
+	void Start()
+	{
+		_spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+	}
+
+
+	public virtual void SetActiveIcon(int spriteIndex)
+	{
+		_spriteRenderer.sprite = SpriteLibrary[spriteIndex];
+	}
+}
