dd7c8ace7ea7dd7502c046e03e383cc8362435c7
Zachary White <zach.salter@sojostudios.com>
2013-11-27 17:35:10 -0800

    Reorganizing.

---
+++ Assets/Scripts/Models/Items/IStorable.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Interface for any object that can be stored.
+/// </summary>
+public interface IStorable
+{
+	string Name { get; set; }
+	Sprite Icon { get; set; }
+
+}
