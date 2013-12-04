25226abc7745e119fdfb3f1ff1cb82e2159aecdb
Zachary White <zach.salter@sojostudios.com>
2013-12-03 17:27:08 -0800

    Beginning of input manager class.

---
+++ Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : Manager
+{
+#region Properties
+	private Vector3 _mousePosition;
+	public Vector3 MousePosition
+	{
+		get { return _mousePosition; }
+		set { _mousePosition = value; }
+	}
+
+	private bool _mouseLeftButtonDown;
+	public bool IsLeftMouseButtonDown
+	{
+		get { return _mouseLeftButtonDown; }
+		set { _mouseLeftButtonDown = value; }
+	}
+#endregion
+
+
+	void Update()
+	{
+		// Update the position of the mouse.
+		MousePosition = Input.mousePosition;
+	}
+}
