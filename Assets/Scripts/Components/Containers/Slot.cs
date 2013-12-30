4e0c69106f5bf64606604f02f0e086a8c91880c7
Zachary White <zack@globaldragongames.com>
2013-12-30 08:31:30 -0800

    Implementing MonoConcept library.

--- Assets/Scripts/Components/Containers/Slot.cs
+++ Assets/Scripts/Components/Containers/Slot.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using MonoConcept;
 
 [RequireComponent (typeof(SpriteRenderer))]
 public abstract class Slot : MonoBehaviour 
---
+++ Assets/Scripts/Constants/CONTAINERS.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class CONTAINERS
+{
+	public const string INVENTORY = "_Inventory";
+}
