197074b2d1975ea0c25343d033d8d56c99d7429d
zachwhite <zach.white@sojostudios.com>
2014-01-03 19:54:19 -0800

    New Compound object for handling molecular compounds.

--- Assets/Scripts/Models/Items/Matter/Compound.cs
+++ Assets/Scripts/Models/Items/Matter/Compound.cs
@@ -1,1 +1,36 @@
+using System.Collections.Generic;
 
+public sealed class Compound : IMatter
+{
+#region Properties
+	private int _CompoundID;
+	public int CompoundID
+	{
+		get { return _CompoundID; }
+		set { _CompoundID = value; }
+	}
+	
+	private string _Name;
+	public string Name
+	{
+		get { return _Name; }
+		set { _Name = value; }
+	}
+
+	private List<string[]> _Molecules;
+	public List<string[]> Molecules
+	{
+		get { return _Molecules; }
+		set { _Molecules = value; }
+	}
+
+	private string[] _Atoms;
+	public string[] Atoms
+	{ 
+		get { return _Atoms; } 
+		set { _Atoms = value; }
+	}
+#endregion
+
+
+}
