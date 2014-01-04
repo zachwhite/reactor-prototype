f2971896b8954f940c13c457f0d262ac6598e590
zachwhite <zach.white@sojostudios.com>
2014-01-03 18:07:35 -0800

    StringParser: Can parse numbers and parenthesis. Separate Split method.

--- Assets/Scripts/Utilities/StringParser.cs
+++ Assets/Scripts/Utilities/StringParser.cs
@@ -7,19 +7,91 @@ public static class StringParser
 {
 	public const char SPLIT_CHAR = ((char)007);
 
+
 	/// <summary>
-	/// Splits string at each capital letter.
+	/// Split a string prepared by a StringParser method.
+	/// </summary>
+	/// <param name="inputString">Input string.</param>
+	public static string[] Split(string inputString)
+	{
+		return inputString.Split (SPLIT_CHAR);
+	}
+
+
+	/// <summary>
+	/// Prepare string to be split at each capital letter.
 	/// </summary>
 	/// <returns>String array of split elements.</returns>
 	/// <param name="inputString">The string to split.</param>
-	public static string[] SplitCapitalLetters(string inputString)
+	public static string CapitalLetters(string inputString)
 	{
 		StringBuilder stringBuilder = new StringBuilder();
-		foreach (char character in inputString) {
-			if (Char.IsUpper(character) && stringBuilder.Length > 0) stringBuilder.Append(SPLIT_CHAR);
+		foreach (char character in inputString) 
+		{
+			if (Char.IsUpper(character) && 
+			    stringBuilder.Length > 0)
+			{
+				stringBuilder.Append(SPLIT_CHAR);
+			}
+
 			stringBuilder.Append(character);
 		}
 
-		return stringBuilder.ToString ().Split (SPLIT_CHAR);
+		return stringBuilder.ToString ();
+	}
+
+
+	public static string Numbers(string inputString)
+	{
+		StringBuilder stringBuilder = new StringBuilder();
+		foreach (char character in inputString) 
+		{
+			if (Char.IsDigit(character) && 
+			    stringBuilder.Length > 0)
+			{
+				stringBuilder.Append(SPLIT_CHAR);
+			}
+			
+			stringBuilder.Append(character);
+		}
+		
+		return stringBuilder.ToString ();
+	}
+
+
+	public static string Parenthesis (string inputString)
+	{
+		StringBuilder stringBuilder = new StringBuilder();
+
+		int pass = 0;
+		foreach (char character in inputString) 
+		{
+			pass++;
+
+			if (character == '(' && 
+			    stringBuilder.Length > 0)
+			{
+				stringBuilder.Append(SPLIT_CHAR);
+				stringBuilder.Append(character);
+			}
+			
+
+
+			else if (character == ')')
+			{
+				stringBuilder.Append (SPLIT_CHAR);
+				stringBuilder.Append(character);
+
+				if (pass < inputString.Length)
+					stringBuilder.Append(SPLIT_CHAR);
+			}
+
+			else
+			{
+				stringBuilder.Append(character);
+			}
+		}
+		
+		return stringBuilder.ToString ();
 	}
 }
